b86af5253602fc9694bae39c664452cc9387b033
Elmir Ahadov <elmir.ahadov@yahoo.com>
2019-08-19 16:48:28 +0400

    Add files via upload

---
+++ app/src/main/java/com/view/expo2020/Model/QuizQuestions.java
@@ -0,0 +1,65 @@
+package com.view.expo2020.Model;
+
+public class QuizQuestions {
+    private String Question, answerA, answerB, answerC, answerD, correctAnswer;
+
+    public QuizQuestions(){
+    }
+
+    public QuizQuestions(String question, String answerA, String answerB, String answerC, String answerD, String correctAnswer) {
+        Question = question;
+        this.answerA = answerA;
+        this.answerB = answerB;
+        this.answerC = answerC;
+        this.answerD = answerD;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestion() {
+        return Question;
+    }
+
+    public void setQuestion(String question) {
+        Question = question;
+    }
+
+    public String getAnswerA() {
+        return answerA;
+    }
+
+    public void setAnswerA(String answerA) {
+        this.answerA = answerA;
+    }
+
+    public String getAnswerB() {
+        return answerB;
+    }
+
+    public void setAnswerB(String answerB) {
+        this.answerB = answerB;
+    }
+
+    public String getAnswerC() {
+        return answerC;
+    }
+
+    public void setAnswerC(String answerC) {
+        this.answerC = answerC;
+    }
+
+    public String getAnswerD() {
+        return answerD;
+    }
+
+    public void setAnswerD(String answerD) {
+        this.answerD = answerD;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+}
